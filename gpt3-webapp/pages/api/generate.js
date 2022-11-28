761c2318480d1b08636ea7900ace329361f6d9f5
Pluto <nickjareddelgado@gmail.com>
2022-11-27 19:33:55 -0700

    Implement prompt chaining

--- gpt3-webapp/pages/api/generate.js
+++ gpt3-webapp/pages/api/generate.js
@@ -7,7 +7,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 
-const basePromptPrefix = "Write a very detailed pitch to a VC for a startup that includes the problems it is solving, the detailed solutions, market size, and the business plan. This start-up is building a:\n";
+const basePromptPrefix = "Prompt: Write a very detailed pitch to a VC for a startup that includes the problems it is solving, the detailed solutions, market size, and the business plan. This start-up is building a:\n";
 const generateAction = async (req, res) => {
     //Run our first prompt
     console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
@@ -21,7 +21,21 @@ const generateAction = async (req, res) => {
 
     const basePromptOutput = baseCompletion.data.choices.pop();
 
-    res.status(200).json({ output: basePromptOutput} );
+    const secondPrompt = `
+    pitch: ${basePromptOutput.text}\n
+    prompt: ${req.body.userInput}\n
+    pitch:\n
+    `
+    const secondPromptCompletion = await openai.createCompletion({
+        model: 'text-davinci-002',
+        prompt: `${secondPrompt}`,
+        temperature: 0.777,
+        max_tokens: 777,
+    });
+
+    const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+    res.status(200).json({ output: secondPromptOutput} );
 };
 
 export default generateAction;
