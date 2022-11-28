e7cd52e0416579c159f3db1b91012d688442040b
Pluto <nickjareddelgado@gmail.com>
2022-11-27 18:32:04 -0700

    Added GPT-3 output to front-end after API call

--- gpt3-webapp/pages/index.js
+++ gpt3-webapp/pages/index.js
@@ -63,6 +63,18 @@ const Home = () => {
               </div>
             </a>
           </div>
+          {apiOutput && (
+            <div className='output'>
+              <div className='output-header-container'>
+                <div className='output-header'>
+                  <h3>Output</h3>
+                </div>
+              </div>
+              <div className='output-content'>
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+          )}
           </div>
       </div>
       <div className="badge-container grow">
