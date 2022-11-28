bdbdee94661d75fe645f6dbb11c300e9a0a1ad6b
Pluto <nickjareddelgado@gmail.com>
2022-11-27 18:17:55 -0700

    Created serverless function to call OpenAI with our user input

--- gpt3-webapp/pages/api/generate.js
+++ gpt3-webapp/pages/api/generate.js
@@ -5,3 +5,23 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
+
+
+const basePromptPrefix = "";
+const generateAction = async (req, res) => {
+    //Run our first prompt
+    console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
+
+    const baseCompletion = await openai.createCompletion({
+        model: 'text-davinci-002',
+        prompt: `${basePromptPrefix}${req.body.userInput}`,
+        temperature: 0.7,
+        max_tokens: 250,
+    });
+
+    const basePromptOutput = baseCompletion.data.choices.pop();
+
+    res.status(200).json({ output: basePromptOutput} );
+};
+
+export default generateAction;
