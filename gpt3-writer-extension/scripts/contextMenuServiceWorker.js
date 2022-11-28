d260d1e2e47ac409517faa9b0a38d2cad724c4f2
Pluto <nickjareddelgado@gmail.com>
2022-11-27 20:25:56 -0700

    Created messages for service worker

--- gpt3-writer-extension/scripts/contextMenuServiceWorker.js
+++ gpt3-writer-extension/scripts/contextMenuServiceWorker.js
@@ -1,40 +1,58 @@
 const getKey = () => {
-    return new Promise((resolve, reject) => {
-        chrome.storage.local.get(['openai-key'], (result) => {
-          if (result['openai-key']) {
-            const decodedKey = atob(result['openai-key']);
-            resolve(decodedKey);
-          }
-        });
-      });
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get(["openai-key"], (result) => {
+      if (result["openai-key"]) {
+        const decodedKey = atob(result["openai-key"]);
+        resolve(decodedKey);
+      }
+    });
+  });
+};
+
+const sendMessage = (content) => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const activeTab = tabs[0].id;
+
+    chrome.tabs.sendMessage(
+      activeTab,
+      { message: "inject", content },
+      (response) => {
+        if (response.status === "failed") {
+          console.log("injection failed");
+        }
+      }
+    );
+  });
 };
 
 const generate = async (prompt) => {
-    //Get key from storage
-    const key = await getKey();
-    const url = 'https://api.openai.com/v1/completions';
+  //Get key from storage
+  const key = await getKey();
+  const url = "https://api.openai.com/v1/completions";
 
-    //Call completions endpoint
-    const completionResponse = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${key}`,
-        },
-        body: JSON.stringify({
-            model: 'text-davinci-002',
-            prompt: prompt,
-            max_tokens: 777,
-            temperature: 0.777
-        }),
-    });
+  //Call completions endpoint
+  const completionResponse = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${key}`,
+    },
+    body: JSON.stringify({
+      model: "text-davinci-002",
+      prompt: prompt,
+      max_tokens: 777,
+      temperature: 0.777,
+    }),
+  });
 
-    const completion = await completionResponse.json();
-    return completion.choices.pop();
+  const completion = await completionResponse.json();
+  return completion.choices.pop();
 };
 
 const generateCompletionAction = async (info) => {
   try {
+    sendMessage('generating...');
+
     const { selectionText } = info;
     const prompt = `
         Tell me a story about ${selectionText} in as much detail as you can.\n
@@ -48,11 +66,13 @@ const generateCompletionAction = async (info) => {
     story: ${baseCompletion.text}\n
     prompt: Tell me a story about ${selectionText} in as much detail as you can.\n
     story:\n
-    `
+    `;
     const secondPromptCompletion = await generate(secondPrompt);
-    console.log(secondPromptCompletion.text)
+    
+    sendMessage(secondPromptCompletion.text);
   } catch (error) {
     console.log(error);
+    sendMessage(error.toString());
   }
 };
 
